292c59601d410ac19ce9616d587d4051f1a2668a
Jake Rayson <web@growdigital.org>
2017-06-27 08:29:13 +0100

    added icons class to footer id in icons.js

--- build/assets/scripts.js
+++ build/assets/scripts.js
@@ -20,12 +20,13 @@ document.addEventListener("DOMContentLoaded", function() {
  * Big thanks to Florens Verschelde:
  * https://fvsch.com/code/svg-icons/how-to/#section-sprite
  * Check for SVG support. If it exists, Ajax in svg file into js-svg ID div
- * Otherwise, add no-svg class to body tag
+ * and add 'svg' class to body and 'icons' class to footer id
  */
 
 var supportsSvg =  document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image", "1.1");
 
 if (supportsSvg) {
+  // Add svg class to body
   document.body.classList.add("svg");
   var url = "/assets/images/icon.svg"
   var xhr = new XMLHttpRequest();
@@ -33,7 +34,8 @@ if (supportsSvg) {
   xhr.onload = function() {
     if(xhr.status === 200) {
       document.getElementById('js-svg').innerHTML = xhr.responseText;
-      document.getElementById('js-svg').classList.add("icons");
+      // Add icons class to footer
+      document.getElementById('footer').classList.add("icons");
     }
   };
   xhr.open('GET', url, true);
@@ -41,4 +43,11 @@ if (supportsSvg) {
 } else {
   document.body.classList.add("no-svg");
 }
+
+
+/**
+ * TODO:
+ * Apply classes to html rather than body
+ * That way, don't have to use id on footer element
+ */
 
